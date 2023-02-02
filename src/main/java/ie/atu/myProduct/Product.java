d5b9680a147a3d9b696b3aa67a24aa4d89c71865
Patrick Feeney <patrickfeeneytamayo@gmail.com>
2023-02-02 17:57:54 +0000

    Create Product.java

--- src/main/java/ie/atu/myProduct/Product.java
+++ src/main/java/ie/atu/myProduct/Product.java
@@ -1,4 +1,50 @@
 package ie.atu.myProduct;
 
+import java.text.NumberFormat;
+
 public class Product {
+    private String code;
+    private String description;
+    private double price;
+    protected static int count = 0;
+
+    public Product() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPriceFormatted() {
+        NumberFormat myCurrency = NumberFormat.getCurrencyInstance();
+        return myCurrency.format(price);
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+    public static int getCount() {
+        return count;
+    }
 }
