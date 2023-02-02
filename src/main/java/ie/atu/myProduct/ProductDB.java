5edc216bc8bd1660b97bd4d14fac5347454950c2
Patrick Feeney <patrickfeeneytamayo@gmail.com>
2023-02-02 18:00:25 +0000

    Create but not finish ProductDb

--- src/main/java/ie/atu/myProduct/ProductDB.java
+++ src/main/java/ie/atu/myProduct/ProductDB.java
@@ -1,4 +1,11 @@
 package ie.atu.myProduct;
 
 public class ProductDB {
+    public static Product getProduct(String productCode) {
+        Product p = null;
+
+        if (productCode.equalsIgnoreCase("java")) {
+            Book myBook = new Book();
+        }
+    }
 }
